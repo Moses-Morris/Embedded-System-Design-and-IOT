3d327ebd617d169c6f778a4e41d82d08fb006cce
Moses-Morris <mosesmorrisdev@gmail.com>
2022-11-09 21:40:27 +0300

    PIC controller LED file

---
+++ PIC Controllers/LED/switch.c
@@ -0,0 +1,25 @@
+#include<pic.h>
+CONFIG (0X3F72);
+void delayms(unsigned int);
+void delayms(unsigned int ms)
+(
+	int cnt;
+	while(ms >0)
+	(
+		ms--;
+		for(cnt=0;cnt<220;cnt ++);
+	}
+}
+void main()
+(
+	delayms(10);
+	TRISB=0XFF;
+	TRISC=0X00;
+	PORTC=0X00;
+	delayms(10);
+	//delayms(10);
+	while(1)
+	(
+	PORTC PORTB;
+	delayms(500);
+}
