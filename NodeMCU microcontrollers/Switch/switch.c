46de0a81d9835c971d96360ce57c91da69012a50
Moses-Morris <mosesmorrisdev@gmail.com>
2022-11-14 21:53:00 +0300

    Fix:  Switch file

--- NodeMCU microcontrollers/Switch/switch.c
+++ NodeMCU microcontrollers/Switch/switch.c
@@ -1,13 +1,16 @@
-const int buttonpin1 = DO;
+const int buttonpin1 = D0; //
 const int buttonpin2 = D1;
 const int buttonpin3 = D2;
 const int ledpin1 = D3;
 const int ledpin2 = D4;
 const int ledpin3 = D5;
 
+
+//button status
 int buttonstate1=0;
 int buttonstate2=0;
 int buttonstate3=0;
+
 void setup() {
     // put your setup code here, to run once:
     pinMode(ledpin1, OUTPUT);
@@ -29,6 +32,29 @@ void loop()
     {
     digitalWrite(ledpin1, LOW);
     }
+
+
+    buttonstate2 = digitalRead(buttonpin2);
+    if (buttonstate2 == HIGH)
+    {
+    digitalWrite(ledpin2, HIGH);
+    }
+    else
+    {
+    digitalWrite(ledpin2, LOW);
+    }
+
+
+buttonstate3 = digitalRead(buttonpin3);
+if (buttonstate3 == HIGH)
+{
+digitalWrite(ledpin3, HIGH);
+}
+else
+{
+digitalWrite(ledpin3, LOW);
+}
+
 
 
 }
