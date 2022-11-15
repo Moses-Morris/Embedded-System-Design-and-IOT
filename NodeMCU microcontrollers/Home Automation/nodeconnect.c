465e51768ad401fa445c50c60c8f3c8794b01212
Moses-Morris <mosesmorrisdev@gmail.com>
2022-11-15 20:51:28 +0300

    Fix : Home automation node MCU connection

--- NodeMCU microcontrollers/Home Automation/nodeconnect.c
+++ NodeMCU microcontrollers/Home Automation/nodeconnect.c
@@ -4,15 +4,15 @@
 #include <BlynkSimpleEsp8266.h>
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon) .
-char auth[] = "hVFJVeeBQIAmjfjob-G20YhQWt.JUYyEM";
+char auth[] = "qwertyu"; //token from blynk app
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "ZTE-adde78";
-char pass[] = "885dfbad";
+char ssid[] = "ZTE-adde78"; //wifi name
+char pass[] = "885dfbad"; //wifi password
 void setup()
 {
     // Debug console
-    Serial.begin(9600);
+    Serial.begin(9600); //baud rate
     Blynk.begin(auth, ssid, pass);
 }
